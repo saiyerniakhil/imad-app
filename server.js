150c2e5f8c096152ed89587003f3fd0c2abf1a1d
Sai Yerni Akhil <32711472+saiyerniakhil@users.noreply.github.com>
2018-02-16 11:07:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.get('/', function (req, res) {
 
 app.get('/:articleName', function (req,res){
      var articleName = req.params.articleName;
-     res.send(createTemplate(articles(articleName)));
+     res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/article-two', function (req,res){
