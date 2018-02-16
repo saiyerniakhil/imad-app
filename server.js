56d6f33fcfef69da750c671c55feea4540cb8212
Sai Yerni Akhil <32711472+saiyerniakhil@users.noreply.github.com>
2018-02-16 09:53:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/article-two', function (req,res){
 });
 
 app.get('/article-three', function (req,res){
-    res.send("This is Article Three ");
+    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
