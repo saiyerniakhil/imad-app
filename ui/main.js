cc5279b96ad19463ce0a31b5b323ff733ecdde5c
Sai Yerni Akhil <32711472+saiyerniakhil@users.noreply.github.com>
2018-02-19 22:38:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,10 @@ console.log('Loaded!');
 //change the text in main text id
 var element = document.getElementById("main-text");
 element.innerHTML = " New Values";
+
+// move the madi img
+
+var img = document.getElementById("madi");
+img.onclick = function () {
+     img.style.marginRight = '100px';
+}
