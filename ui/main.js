202c2ef60cbe433df628dada6f4ca8f1d4c85ce3
Sai Yerni Akhil <32711472+saiyerniakhil@users.noreply.github.com>
2018-02-19 22:43:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,11 @@ element.innerHTML = " New Values";
 // move the madi img
 
 var img = document.getElementById("madi");
+var marginLeft = 0;
+function moveRight() {
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
 img.onclick = function () {
-     img.style.marginRight = '100px';
+     var interval = setInterval(moveRight,50);
 }
