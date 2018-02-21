01970c3ea47de5ef204dc869008915c935c3c20a
Sai Yerni Akhil <32711472+saiyerniakhil@users.noreply.github.com>
2018-02-21 17:26:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -68,8 +68,9 @@ app.get('/:articleName', function (req,res){
      res.send(createTemplate(articles[articleName]));
 });
 
-var counter = 0;
+
 app.get('/counter',function (req,res){
+    var counter = 0;
     counter = counter + 1;
     res.send(`CLicks ${counter.toString()}`);
 });
