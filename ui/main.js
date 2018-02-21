7a3c480e75c75299ee6eb98282fc06e88a761257
Sai Yerni Akhil <32711472+saiyerniakhil@users.noreply.github.com>
2018-02-21 18:22:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,21 +1,21 @@
 var button = document.getElementById('counter');
 
 var counter = 0;
- button.onclick = function () {
+    button.onclick = function () {
     //create a request object 
-    var request = new XMLHttpRequest();
+        var request = new XMLHttpRequest();
     //capture the counter and store it in a variable 
-    request.onreadystatechange = function () {
-        if(request.readyState == XMLHttpRequest.DONE){
-         var counter = request.responseText;
-         var span = document.getElementById('count');
-         span.innerHTML = counter.toString();
-        }
+            request.onreadystatechange = function () {
+                if(request.readyState == XMLHttpRequest.DONE) {
+                    var counter = request.responseText;
+                    var span = document.getElementById('count');
+                    span.innerHTML = counter.toString();
+            }
         // if XMLHttpRequest is NOT Done
+        };
     };
- };
     
     //make the request
-    request.open('GET','http://http://akhilsai831.imad.hasura-app.io/counter',true);
+    request.open('GET','http://akhilsai831.imad.hasura-app.io/counter',true);
     request.send(null);
- };
+};
