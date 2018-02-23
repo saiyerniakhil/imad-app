be2bc8d66d1c97a624c0047c5407c43884988161
Sai Yerni Akhil <32711472+saiyerniakhil@users.noreply.github.com>
2018-02-23 14:16:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -35,7 +35,8 @@ submit.onclick = function () {
                 if(request.readyState === XMLHttpRequest.DONE) {
                     // take some action i.e. print count 
                     if(request.status === 200) {
-                        var names = ['name1','name2','name3'];
+                        var names = request.responseText;
+                        names = JSON.parse(names);
                         var list= '';
                         for (var i = 0;i<names.length;i++){
                             list += '<li>'+names[i]+'</li>';
