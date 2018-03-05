266757d1f3e2c6da3e73577f9d83d2afb0827234
Sai Yerni Akhil <32711472+saiyerniakhil@users.noreply.github.com>
2018-03-05 22:24:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -102,19 +102,18 @@ app.get('/submit-name/', function(req ,res){ //URL : submit-name?name=xxxxxx
 });
 
 app.get('articles/:articleName', function (req,res){
-     pool.query("SELECT * FROM article WHERE title ="+req.params.articleName , function (err , result){
+     pool.query("SELECT * FROM article WHERE title ="+req.params.articleName , function (err,result){
         if(err) {
             res.status(500).send(err.toString());
         } else {
             if(result.rows.length === 0 ){
-              res.status(500).send('Article Not Found');
+              res.status(404).send('Article Not Found');
             } else {
                 var articleData = result.rows[0];
                 res.send(createTemplate(articleData));
             }
         }
      });
-     res.send(createTemplate(articleData));
 });
 
 
