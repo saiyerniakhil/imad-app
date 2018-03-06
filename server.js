f344b787aac29169039687fac6c2199a3fdc2830
Sai Yerni Akhil <32711472+saiyerniakhil@users.noreply.github.com>
2018-03-06 22:17:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ app.get('/submit-name/', function(req ,res){ //URL : submit-name?name=xxxxxx
 
 function hash(input ,salt){
     var hashed = crypto.pbkdf2Sync(input, salt, 100000, 64, 'sha512');
-    return hashed.toString('hex');
+    return ["pbkdf2","10000",salt,hashed.toString('hex')].join('$');
 }
 app.get('/hash/:input', function(req,res) {
     var hashedString = hash(req.params.input ,'this-is-some-random-string');
