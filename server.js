47a08e52de1215fad6bf77eba6732b5f88de7cf1
Sai Yerni Akhil <32711472+saiyerniakhil@users.noreply.github.com>
2018-03-09 21:09:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var Pool = require('pg').Pool;
 var path = require('path');
 var crypto = require('crypto');
+var bodyParser = require('body-parser');
     var config = {
         user: 'akhilsai831',
         database : 'akhilsai831',
@@ -42,11 +43,25 @@ var htmlTemplate = `
 
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.post('/create-user',function (req,res){
+    var username = req.body.username;
+    var passowrd = req.body.password;
+    var salt = crypto.getRandomBytes(128).toString('hex');
+    var dbString = hash(password,salt);
+    pool.query('INSERT INTO "user" (username,password) VALUES ($1,$2)',[username , dbString], function (err, result){
+         if(err) {
+            res.status(500).send(err.toString());
+        } else {
+            res.send('user successfully created'+ username);
+        }
+    });
+});
 var pool = new Pool(config); 
 app.get('test-db',function(req,res){
     //make a select response
